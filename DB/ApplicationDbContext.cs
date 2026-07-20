9247e3607adcc78e3a0cf379f075fd75c7601df2
school_app <eng.rakesh.thakral@gmail.com>
2026-07-20 22:05:12 +0530

    Initial commit

---
+++ DB/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FacultyApi.Data
+
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>  options):base(options)
+        { 
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+using FacultyApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ================= DB =================
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// ================= SERVICES =================
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddHttpClient();
+// ================= GPS SERVICES =================
+
+
+
+
+// ================= CORS =================
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            // SetIsOriginAllowed(_ => true) allows all origins dynamically,
+            // which safely permits you to chain .AllowCredentials()
+            policy.SetIsOriginAllowed(_ => true)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials(); // Required for SignalR streaming
+        });
+});
+
+var app = builder.Build();
+
+
+// ================= MIDDLEWARE =================
+app.UseWebSockets();
+
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+app.UseAuthorization();
+// ================= SWAGGER =================
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+// ================= API =================
+app.MapControllers();
+
+// ================= SIGNALR HUB =================
+
+app.Run();
+
