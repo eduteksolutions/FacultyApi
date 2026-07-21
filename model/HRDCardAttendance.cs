b4dcfc8b08431d495262bb1a16af93c74adef587
school_app <eng.rakesh.thakral@gmail.com>
2026-07-21 22:57:57 +0530

    update

--- model/HRDCardAttendance.cs
+++ model/HRDCardAttendance.cs
@@ -9,34 +9,34 @@ namespace FacultyApi.model
     public class HRDCardAttendance
     {
         [Key]
-      
+
         public int empCode { get; set; }
 
         public DateTime aDate { get; set; }
 
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         public DateTime? inTime { get; set; }
 
-        public string eTimings { get; set; }
+        public string? eTimings { get; set; }
 
-        public string LeaveType { get; set; }
+        public string? LeaveType { get; set; }
 
-        public string LoginName { get; set; }
+        public string? LoginName { get; set; }
 
         public DateTime? lUserDt { get; set; }
 
         public int UserID { get; set; }
 
-        public string reason { get; set; }
+        public string? reason { get; set; }
 
-        public string facultyName { get; set; }
+        public string? facultyName { get; set; }
 
-        public string ltype { get; set; }
+        public string? ltype { get; set; }
 
-        public string longitude { get; set; }
+        public string? longitude { get; set; }
 
-        public string latitude { get; set; }
+        public string? latitude { get; set; }
 
         public DateTime? eintime { get; set; }
 
