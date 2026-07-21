47d9dd9dab7dfcd53d52e78e6918fa080dc02c22
school_app <eng.rakesh.thakral@gmail.com>
2026-07-21 23:51:37 +0530

    mode update'

--- model/PunchInRequest.cs
+++ model/PunchInRequest.cs
@@ -3,13 +3,21 @@
     public class PunchInRequest
     {
         public int FacultyCd { get; set; }
+
         public DateTime S_Date { get; set; }
-        public string Status { get; set; }
-        public string Reason { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? Reason { get; set; }
+
         public int UserId { get; set; }
-        public string FacultyName { get; set; }
-        public string Longitude { get; set; }
-        public string Latitude { get; set; }
-        public string EInTime { get; set; }
+
+        public string? FacultyName { get; set; }
+
+        public string? Longitude { get; set; }
+
+        public string? Latitude { get; set; }
+
+        public string? EInTime { get; set; }
     }
 }
